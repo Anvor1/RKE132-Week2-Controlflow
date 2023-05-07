644243b8c7a533d21870c1c0d62c61816e5271ef
Anvor <anvor.arak@karlstorz.com>
2023-05-07 22:10:42 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus palub kasutajal selekteerida oma sugu (m/f)
+//rakendus  palub sisetada kasutajanime
+//lähtudes kasutajavalikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome Mr. [kasutaja perekonnanimi] / "Welcome, Ms [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please , select your gender (m/f):");
+
+char userGender = Char.Parse (Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+Console.WriteLine("Please, enter your name");
+
+string userLastName = Console.ReadLine ();
+
+    string userLastname = Console.ReadLine ();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome! {userLastName}");
+}
+
---
+++ UserAgeValidation2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus palub kasutajal sisetada enda vanuse
+//kui kasutaja vanus on vähem kui 13, siis konsoolis kuvatakse:
+//"You are too young to use Instagram"
+//muul juhul
+//konsoolis kuvatakse "Welcome to instagram"
+
+Console.WriteLine("Enter your age");
+
+int userAge = Int32.Parse(Console.ReadLine()); //"13" - heap, 13- stack-int tüüpi
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+//else if (userAge == 13) ka variant kui tahad lisaridu koodi
+//{
+    //Console.WriteLine("Welcome to Instagram!");
+//}
+else // if käsuga saab ka aga pole vaja
+{
+    Console.WriteLine("You are too young to use Instagram");
+}
